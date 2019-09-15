3eed85e4e857b07738645ef0708a67365cd2d2a2
adamjgordon28 <adamjgordon24@gmail.com>
2019-09-15 11:15:34 -0400

    starting to replicate cards

---
+++ src/components/TellTaleProjectCard.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import '../App.css';
+
+class WWNProjectCard extends Component {
+  render(){
+    return(<div class="ui card">
+  <div class="image">
+    <img src="adam-gordon-burner-hat.png"/>
+  </div>
+  <div class="content">
+  Words With Nerds
+    <div class="meta">
+      <span class="date">Created in April 2019</span>
+    </div>
+    <div class="description">
+      This is an unusually fun word game!
+    </div>
+  </div>
+  <div class="extra content">
+    <a>
+      <i class="user icon"></i>
+      1000+ plays
+    </a>
+  </div>
+</div>)
+  }
+}
+
+
+export default WWNProjectCard
